669b4ad624b5e35d43766215631b74a56cce24dd
bilgedemirkaya <bilgedemirkaya@users.noreply.github.com>
2020-06-01 20:33:44 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -4,36 +4,41 @@
 #include <ctype.h>
 #include <math.h>
 
- int main(void)
+int main(void)
 {
+    // get a text from user and grade it
     int i = 0; 
     string text = get_string("Text: ");
-   int n = strlen(text);
-   
-   for (int c = 0; c < n; c++ )
-   {
-     
-     if (text[c] >= 'a' && text[c] <= 'z')
-     {
-         i += 1;
-     }
-     else if(text[c] >= 'A' && text[c] <= 'Z')
-     {
-         i += 1;
-     }
-    
-   }
-   //printf("%i letter(s)\n", i);
-    
-    int w = 1;
-    for (int c = 1; c < n ; c++)
+    int n = strlen(text);
+    // find out the letters used in text in total
+    for (int c = 0; c < n; c++)
     {
-        if (((text[c] == ' ') && (text [c + 1] >= 'a') && (text[c + 1] <= 'z')) || ((text[c] == ' ') && (text [c + 1] >= 'A') && (text[c + 1] <= 'Z')) )
+        if (text[c] >= 'a' && text[c] <= 'z')
+        {
+            i += 1;
+            
+        }
+        else if (text[c] >= 'A' && text[c] <= 'Z')
+        {
+            i += 1;
+            
+        }
+        
+    }
+    
+    //printf("%i letter(s)\n", i);
+    // find out the word seperated by ' ' in text in total
+    int w = 1;
+    for (int c = 2; c < n ; c++)
+    {
+        if (((text[c] == ' ') && (text [c + 1] >= 'a') && (text[c + 1] <= 'z')) || ((text[c] == ' ') && (text [c + 1] >= 'A')
+                && (text[c + 1] <= 'Z')))
         {
             w += 1;
         }
     }
-   //printf("%i word(s)\n", w);
+    //printf("%i word(s)\n", w);
+    //find out the sentences in text in total 
     
     int s = 0;
     for (int c = 0; c < n; c++)
@@ -45,7 +50,7 @@
         
     }
     //printf("%i sentences\n", s);
-    
+    // convert integer values to float type to get the right result
     float L;
     L = ((double)i / (double)w) * 100;
     
@@ -55,16 +60,18 @@
     //printf("%f\n", L);
     
     //printf("%f\n", S);
+    //used the formula for grading the text
     
     float index;
     
     index = (0.0588 * L) - (0.296 * S) - 15.8;
     
     index = round(index);
+    // print the result
     
     if (index < 1)
     {
-    printf("Before Grade 1\n");
+        printf("Before Grade 1\n");
     }
     else if (index > 16)
     {
@@ -72,7 +79,7 @@
     }
     else
     {
-    printf("Grade %1.f\n", index);
+        printf("Grade %1.f\n", index);
     }
     return 0;
 }
